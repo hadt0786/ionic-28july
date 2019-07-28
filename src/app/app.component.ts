216c8428c88f3c9f3f415681ad632936dda87fd0
Akash <akash@zibtek.in>
2019-07-29 00:20:34 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,17 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyBWWHxBaFpJxN9doSgH3ixI6r5XwgUTnag",
+  authDomain: "ionic-28jul.firebaseapp.com",
+  databaseURL: "https://ionic-28jul.firebaseio.com",
+  projectId: "ionic-28jul",
+  storageBucket: "",
+  messagingSenderId: "987668034549",
+  appId: "1:987668034549:web:56cfd108f6aa7b13"
+};
 
 @Component({
   selector: 'app-root',
@@ -23,5 +34,6 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+    firebase.initializeApp(config);
   }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { AlertController, LoadingController } from '@ionic/angular';
+import { Router, ActivatedRoute } from '@angular/router';
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +9,68 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  infos = [];
+  ref = firebase.database().ref('infos/');
 
+  constructor(public router: Router,
+    public loadingController: LoadingController) {
+
+    this.ref.on('value', resp => {
+
+      this.infos = [];
+      this.infos = snapshotToArray(resp);
+
+    });
+
+
+  }
+
+  addInfo() {
+    this.router.navigate(['/add-info']);
+  }
+
+  edit(key) {
+    this.router.navigate(['/edit/' + key]);
+  }
+
+  alertController: AlertController;
+
+  async delete(key) {
+    const alert = await this.alertController.create({
+      header: 'Conform',
+      message: ' Are you sure want to delete this info?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('cancel');
+          }
+        },
+        {
+          text: 'Okay',
+          handler: () => {
+            firebase.database().ref('/infos/' + key).remove();
+          }
+        }
+      ]
+    });
+    await alert.present();
+
+  }
+
+
+}
+
+export const snapshotToArray = snapshot => {
+  let returnArr = [];
+  snapshot.forEach(childSnapshot => {
+
+    let item = childSnapshot.val();
+    item.key = childSnapshot.key;
+    returnArr.push(item);
+
+  });
+  return returnArr;
 }
